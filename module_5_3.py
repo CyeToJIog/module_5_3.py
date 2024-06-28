ff637672eeca902e0936e3543164d1407bf3c008
CyeToJIog <168110110+CyeToJIog@users.noreply.github.com>
2024-06-28 12:52:14 +0300

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,61 @@
+class House:
+    def __init__(self, name, floors):
+        self.name = name
+        self.floors = floors
+
+    def __eq__(self, other):
+        return self.floors == other.floors if isinstance(other, House) else False
+
+    def __lt__(self, other):
+        return self.floors < other.floors if isinstance(other, House) else NotImplemented
+
+    def __le__(self, other):
+        return self.floors <= other.floors if isinstance(other, House) else NotImplemented
+
+    def __gt__(self, other):
+        return self.floors > other.floors if isinstance(other, House) else NotImplemented
+
+    def __ge__(self, other):
+        return self.floors >= other.floors if isinstance(other, House) else NotImplemented
+
+    def __ne__(self, other):
+        return self.floors != other.floors if isinstance(other, House) else True
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.floors += value
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.floors}"
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2)  # __eq__
+
+h1 = h1 + 10  # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10  # __iadd__
+print(h1)
+
+h2 = 10 + h2  # __radd__
+print(h2)
+
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
